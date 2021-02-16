bde5e46c529607f7a5ca16b18a7cae88a36b1b68
Condei9 <coge9989@gmail.com>
2021-02-16 17:14:42 +0200

    Quick test, added some lines to print all the words in a text that start with uppercase

--- interviubd.py
+++ interviubd.py
@@ -1,1 +1,14 @@
-print("1234")
+import sys
+#data input
+f = open(sys.argv[1], "r")
+
+k = sys.argv[2]
+
+for line in f:
+    word = line.split()
+    for k in word:
+        if k[0].isupper():
+            print(k)
+
+
+f.close()
