5d7bf3186a4d50eb0eb23903b2b7bf19f010f5b1
Condei9 <coge9989@gmail.com>
2021-02-16 17:43:46 +0200

    Pretty primitive(but functional) version of the program. Using a default dict I managed to count and sort the apparitions of all the words that start with uppercase. Runs pretty fast through 600k words

--- interviubd.py
+++ interviubd.py
@@ -1,14 +1,36 @@
 import sys
 #data input
 f = open(sys.argv[1], "r")
+k = int(sys.argv[2])
 
-k = sys.argv[2]
+###########
+#----------printing all the words that start with uppercase
+#for line in f:
+#    word = line.split()
+#    for m in word:
+#        if m[0].isupper():
+#            print(m)
+#            k = k+1
+############
 
-for line in f:
-    word = line.split()
-    for k in word:
-        if k[0].isupper():
-            print(k)
+from collections import defaultdict
+from operator import itemgetter
+import time
+start_time = time.time()
 
+def process(f):
+    apparitions = defaultdict(int)
+    f.seek(0)
+    for line in f:
+        words = line.split()
+        for m in words:
+            if m[0].isupper():
+                apparitions[m] += 1
+    return sorted(apparitions.items(), key=itemgetter(1), reverse=True)
 
+x = process(f)
+for a in range(k):
+    print(x[a])
+
+print("%s secunde" % (time.time() - start_time))
 f.close()
