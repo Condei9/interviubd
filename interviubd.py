163763ed34cbca78872bd756edadff1da32f3046
Condei9 <coge9989@gmail.com>
2021-02-16 23:16:12 +0200

    Added support for multiple-word names

--- interviubd.py
+++ interviubd.py
@@ -16,21 +16,29 @@ k = int(sys.argv[2])
 #            k = k+1
 ############
 import nltk
+nltk.download("punkt")
+nltk.download("stopwords")
 from collections import defaultdict
 from operator import itemgetter
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize 
+
 
 stop_words = set(stopwords.words('english'))
 
 def process(f):
     apparitions = defaultdict(int)
     f.seek(0)
+    comp = ""
     for line in f:
-        words = word_tokenize(f.readline())
+        words = word_tokenize(line)
         for m in words:
             if m[0].isupper() and m.lower() not in stop_words:
-                apparitions[m] += 1
+                comp += m + " "
+            elif comp != "":
+                comp = comp[:-1]
+                apparitions[comp] += 1
+                comp = ""
     return sorted(apparitions.items(), key=itemgetter(1), reverse=True)
 
 x = process(f)
